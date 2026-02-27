df34f7b12be8c6639d32e1044e44014134546802
Anton Solonikov <antonsolonikov@gmail.com>
2026-02-27 21:35:30 +0300

    Базовый проект Pr03 + примеры

---
+++ AllPr/Example/_example_pr03/002_Arrays_hard/SortingSample/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Wrox.ProCSharp.Arrays;
+
+public record Person(string FirstName, string LastName) : IComparable<Person>
+{
+    public int CompareTo(Person? other)
+    {
+        ArgumentNullException.ThrowIfNull(other);
+
+        int result = LastName.CompareTo(other.LastName);
+        if (result == 0)
+        {
+            result = FirstName.CompareTo(other.FirstName);
+        }
+
+        return result;
+    }
+
+    public override string ToString() => $"{FirstName} {LastName}";
+}
---
+++ AllPr/Example/_example_pr03/002_Arrays_hard/YieldSample/GameMove.cs
@@ -0,0 +1,36 @@
+﻿public class GameMoves
+{
+    private IEnumerator _cross;
+    private IEnumerator _circle;
+
+    public GameMoves()
+    {
+        _cross = Cross();
+        _circle = Circle();
+    }
+
+    private int _move = 0;
+    const int MaxMoves = 9;
+
+    public IEnumerator<IEnumerator> Cross()
+    {
+        while (true)
+        {
+            Console.WriteLine($"Cross, move {_move}");
+            if (++_move >= MaxMoves)
+                yield break;
+            yield return _circle;
+        }
+    }
+
+    public IEnumerator<IEnumerator> Circle()
+    {
+        while (true)
+        {
+            Console.WriteLine($"Circle, move {_move}");
+            if (++_move >= MaxMoves)
+                yield break;
+            yield return _cross;
+        }
+    }
+}
