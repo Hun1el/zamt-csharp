0aac2a379f6bee612413b5180d40963b8bf4208d
Anton Solonikov <antonsolonikov@gmail.com>
2026-02-27 17:48:23 +0300

    Pr01/task1. Вычисление объема цилиндра.

--- Pr01/Pr01/Program.cs
+++ Pr01/Pr01/Program.cs
@@ -8,8 +8,19 @@ namespace Pr01
 {
     internal class Program
     {
+        // Вариант 17
         static void Main(string[] args)
         {
+            //Задание 17
+            Console.Write("Введите радиус основания (см): ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите высоту цилиндра (см): ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double form1 = Math.PI * Math.Pow(radius, 2) * height;
+
+            Console.WriteLine("Объем цилиндра: " + form1 + " см.куб");
         }
     }
 }
