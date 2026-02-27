da16ac7652d9a47b68128cbc69212bf246cc9144
Anton Solonikov <antonsolonikov@gmail.com>
2026-02-27 18:26:23 +0300

    Pr01/task2. Вычисление скорости, с которой бегун пробежал дистанцию.

--- Pr01/Pr01/Program.cs
+++ Pr01/Pr01/Program.cs
@@ -21,6 +21,23 @@ namespace Pr01
             double form1 = Math.PI * Math.Pow(radius, 2) * height;
 
             Console.WriteLine("Объем цилиндра: " + form1 + " см.куб");
+
+            //Задание 16
+            Console.Write("Введите длину дистанции (метров): ");
+            double distance = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите время (минут.секунд): ");
+            double time = Convert.ToDouble(Console.ReadLine());
+
+            int minute = Convert.ToInt32(time / 1);
+            double a = time % 1;
+            int second = Convert.ToInt32(a * 100);
+            int totaltime = minute * 60 + second;
+
+            double speedmeter = distance / totaltime;
+            double speedhour = speedmeter * 3.6;
+
+            Console.WriteLine("Вы бежали со скоростью " + speedhour + "км/час");
         }
     }
 }
