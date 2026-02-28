104aae153b7211c7543cf2eb25b8873163277931
Anton Solonikov <antonsolonikov@gmail.com>
2026-02-28 16:14:08 +0300

    Добавлены задания и примеры для Pr08

---
+++ AllPr/Example/_example_pr08/MethodOverloading/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+//Даны два целых числа A и B(A<B). Вывести все целые числа, 
+//расположенные между данными числами (включая сами эти числа), 
+//в порядке их убывания, а также сумму N этих чисел.
+
+//Написать функцию double Fact(int N) вещественного типа, вычисляющую 
+//значение факториала N! = 1·2·…·N(N > 0 - параметр целого типа; 
+//вещественное возвращаемое значение используется для того, чтобы 
+//избежать целочисленного переполнения при больших значениях N).
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Работа с перегруженными методами *****\n");
+            
+            // Calls int version of Add()
+            //Console.WriteLine(Add(10, 10));
+            
+            // Calls long version of Add()
+            //Console.WriteLine(Add(900000000000, 900000000000));
+            
+            // Calls double version of Add()
+            //Console.WriteLine(Add(4.3, 4.4));
+
+            Console.WriteLine("\nСумма = {0}", Sum(10, 20));
+            Console.WriteLine("\nФакториал = {0}", Fact(0));
+
+            Console.ReadLine();
+        }
+
+        #region Overloaded Add() methods
+        // перегруженные методы
+        static int Add(int x, int y)
+        { 
+		    return x + y; 
+		}
+
+        static double Add(double x, double y)
+        { 
+		    return x + y; 
+		}
+
+        static long Add(long x, long y)
+        { 
+		    return x + y; 
+		}
+
+        static int Sum(int A, int B)
+        {
+            int Result = 0;
+            for(int a = A; a<=B; a++)
+            {
+                Console.Write("{0} ", a);
+                Result += a;
+            }
+
+            return Result;
+        }
+
+        static double Fact(int N)
+        {
+            double Result = 1.0;
+
+            for(int k=1;k <=N;k++)
+            {
+                Result *= k;
+            }
+
+            return Result;
+        }
+
+        #endregion
+    }
+}
---
+++ AllPr/Example/_example_pr08/_ex01_func-array/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = pow(5.0, 2.0);
+
+            int Sum = Addition(1, 2, 3, 4, 5);
+            int[] array = new int[] { 10, 20, 30, 40 };
+            int sum2 = Addition(array);
+
+            AdditionArray(array, 4);
+        }
+
+        static double pow(double A, double B)
+        {
+            return Math.Pow(A,B);
+        }
+
+        // передача параметра с params
+        static int Addition(params int[] integers)
+        {
+            int result = 0;
+            for (int i = 0; i < integers.Length; i++)
+            {
+                result += integers[i];
+            }
+
+            return result;
+        }
+
+        // передача массива
+        static void AdditionArray(int[] integers, int k)
+        {
+            int result = 0;
+            for (int i = 0; i < integers.Length; i++)
+            {
+                result += (integers[i] * k);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
