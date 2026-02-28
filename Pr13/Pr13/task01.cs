4ae06bf68140fc72aea4416e3bb51b52e986c140
Anton Solonikov <antonsolonikov@gmail.com>
2026-02-28 23:17:36 +0300

    Pr13/task1. Класс для нахождения факториала числа

--- Pr13/Pr13/task01.cs
+++ Pr13/Pr13/task01.cs
@@ -8,6 +8,36 @@ namespace Pr13
 {
     class task01
     {
+        private int N;
+        public task01(int N)
+        {
+            this.N = N;
+        }
 
+        private int Result(int N)
+        {
+            int product = 1;
+
+            if (N % 2 == 0)
+            {
+                for (int i = 2; i <= N; i += 2)
+                {
+                    product *= i;
+                }
+            }
+            else
+            {
+                for (int i = 1; i <= N; i += 2)
+                {
+                    product *= i;
+                }
+            }
+
+            return product;
+        }
+        public int Result(bool truefalse = true)
+        {
+            return Result((int)N);
+        }
     }
 }
