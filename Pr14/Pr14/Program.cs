ddb473889429266a66cbb5795c3799ac0c4f721d
Anton Solonikov <antonsolonikov@gmail.com>
2026-02-28 23:51:49 +0300

    Создан пустой проект для Pr14

---
+++ Pr14/Pr14/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вариант 17
+// Название яхты (char), владелец (char), площадь парусов (float), количества мачт (int), стоимость тыс.долл. (float).
+// Поиск по первым трем буквам владельца.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
