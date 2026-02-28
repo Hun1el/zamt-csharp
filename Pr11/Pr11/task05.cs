057708412c57bc07fccae36b484c49e28fdc7567
Anton Solonikov <antonsolonikov@gmail.com>
2026-02-28 22:44:06 +0300

    Pr11/task5. Класс для определения количества слов в строке.

--- Pr11/Pr11/task05.cs
+++ Pr11/Pr11/task05.cs
@@ -8,6 +8,15 @@ namespace Pr11
 {
     class task05
     {
+        public string Replaces(string str)
+        {
+            return str.Replace(' ', '_');
+        }
 
+        public int Count(string str)
+        {
+            string[] words = str.Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
     }
 }
