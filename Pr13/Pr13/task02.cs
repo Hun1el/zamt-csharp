878305ec3ef879357db84df2c728ebd07641233b
Anton Solonikov <antonsolonikov@gmail.com>
2026-02-28 23:32:44 +0300

    Pr13/task2. Класс для нахождения суммы и произведения всех элементов в массиве.

--- Pr13/Pr13/task02.cs
+++ Pr13/Pr13/task02.cs
@@ -8,6 +8,43 @@ namespace Pr13
 {
     class task02
     {
+        private int[] array;
 
+        public task02(int size)
+        {
+            array = new int[size];
+            Random random = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(1, 51);
+            }
+        }
+        public int Sum()
+        {
+            int sum = 0;
+            foreach (int num in array)
+            {
+                sum += num;
+            }
+
+            return sum;
+        }
+
+        public int[] GetArray()
+        {
+            return array;
+        }
+
+        public int Prod()
+        {
+            int prod = 1;
+            foreach (int num in array)
+            {
+                prod *= num;
+            }
+
+            return prod;
+        }
     }
 }
