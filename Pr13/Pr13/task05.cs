aa3540c0019641f7a149e0699328bac6193eff09
Anton Solonikov <antonsolonikov@gmail.com>
2026-02-28 23:46:06 +0300

    Pr13/task5. Класс для вывода подстроки между первой и второй точками.

--- Pr13/Pr13/task05.cs
+++ Pr13/Pr13/task05.cs
@@ -8,6 +8,17 @@ namespace Pr13
 {
     class task05
     {
+        public string Str(string input)
+        {
+            int first = input.IndexOf('.');
+            int second = input.IndexOf('.', first + 1);
 
+            if (first == -1 || second == -1)
+            {
+                return input;
+            }
+
+            return input.Substring(first + 1, second - first - 1);
+        }
     }
 }
