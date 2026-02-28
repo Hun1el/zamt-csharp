71125d50312628d89304f77fb39ea9fcfa11d363
Anton Solonikov <antonsolonikov@gmail.com>
2026-02-28 15:49:39 +0300

    Pr07/task1. Проверка положительности чисел A,B,C.

--- Pr07/Pr07/Program.cs
+++ Pr07/Pr07/Program.cs
@@ -9,6 +9,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,42 @@ namespace Pr07
     {
         static void Main(string[] args)
         {
+            //Задание 1
+            try
+            {
+                Console.WriteLine("Задание 1. Даны три целых числа: A, B, C. Проверить высказывание 'Каждое из чисел A, B, C положительное.'");
+                string inputfile1 = "input1.txt";
+                string outputfile1 = "output1.txt";
 
+                string[] line1 = File.ReadAllLines(inputfile1);
+
+                int A1 = Convert.ToInt32(line1[0]);
+                int B1 = Convert.ToInt32(line1[1]);
+                int C1 = Convert.ToInt32(line1[2]);
+
+                bool plus = (A1 > 0 && B1 > 0 && C1 > 0);
+
+                File.WriteAllText(outputfile1, plus.ToString());
+                Console.WriteLine("Вывод успешно записан в файл!");
+            }
+            catch (FormatException ex1)
+            {
+                Console.WriteLine("Ошибка: неверный формат данных. Проверьте input1.txt. " + ex1.Message);
+                File.WriteAllText("output1.txt", "Ошибка: неверный формат данных. Проверьте input1.txt.");
+            }
+            catch (IndexOutOfRangeException ex2)
+            {
+                Console.WriteLine("Ошибка: " + ex2.Message);
+                File.WriteAllText("output1.txt", "Ошибка: Индекс находился вне границ массива. Проверьте input1.txt.");
+            }
+            catch (DirectoryNotFoundException ex3)
+            {
+                Console.WriteLine("Ошибка: Директория не найдена. " + ex3.Message);
+            }
+            catch (FileNotFoundException ex4)
+            {
+                Console.WriteLine("Ошибка: Файл не найден. " + ex4.Message);
+            }
         }
     }
 }
