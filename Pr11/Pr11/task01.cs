11e6c82f2ee5c15f1f0951723135cbd51131f55f
Anton Solonikov <antonsolonikov@gmail.com>
2026-02-28 20:32:59 +0300

    Pr11/task1. Класс для проверки на палиндром.

--- Pr11/Pr11/task01.cs
+++ Pr11/Pr11/task01.cs
@@ -6,8 +6,39 @@ using System.Threading.Tasks;
 
 namespace Pr11
 {
-    public static class task01
+    class task01
     {
+        int N;
+        bool chislo;
 
+        public task01()
+        {
+            this.N = 0;
+            this.chislo = false;
+        }
+
+        public void Number(int number)
+        {
+            N = number;
+        }
+
+        public void Check()
+        {
+            int first = N / 100;
+            int last = N % 10;
+            chislo = (first == last);
+        }
+
+        public void Result()
+        {
+            if (chislo)
+            {
+                Console.WriteLine(N + " читается одинаково слева направо и справа налево.");
+            }
+            else
+            {
+                Console.WriteLine(N + " НЕ читается одинаково слева направо и справа налево.");
+            }
+        }
     }
 }
