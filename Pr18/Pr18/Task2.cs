6c6c584c6f15d794ee1c26a73968bd8055d582a5
Anton Solonikov <antonsolonikov@gmail.com>
2026-03-01 16:24:11 +0300

    Pr18/task2. Реализована функция нахождения разницы между произведениями первых двух и последних двух цифр числа.

--- Pr18/Pr18/Task2.cs
+++ Pr18/Pr18/Task2.cs
@@ -24,7 +24,18 @@ namespace Pr18
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if (int.TryParse(textBox1.Text, out int n) && n > 999 && n < 10000)
+            {
+                int firstTwo = (n / 1000) * (n / 100 % 10);
+                int lastTwo = (n % 100 / 10) * (n % 10);
+                int difference = firstTwo - lastTwo;
 
+                label2.Text = $"Разница: {difference}";
+            }
+            else
+            {
+                label2.Text = "Введите корректное четырехзначное число.";
+            }
         }
     }
 }
