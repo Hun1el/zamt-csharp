ed9f8c58f422b2c8df90cde7350630ea919e2432
Anton Solonikov <antonsolonikov@gmail.com>
2026-03-01 20:43:33 +0300

    Добавлены задания и примеры для Pr25

---
+++ AllPr/Example/_example_pr25/007 Window/001_DialogsTypes/DialogsTypes/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DialogsTypes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            NewWindow window = new NewWindow("Модальное");
+            window.ShowDialog();
+            // Код после метода ShowDialog выполнится только тогда, кода диалоговое окно закроется.
+            MessageBox.Show("После модального диалога...");
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            NewWindow window = new NewWindow("Немодальное");
+            window.Show();
+            // Код после метода Show выполнится сразу.
+            MessageBox.Show("После немодального диалога...");
+        }
+    }
+}
