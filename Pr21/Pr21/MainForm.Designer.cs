09ceed58efd0a490ce085c3ac42ceda3adfa24e3
Anton Solonikov <antonsolonikov@gmail.com>
2026-03-01 19:13:26 +0300

    Pr17. Добавлен проект Pr21 с созданием документа в MS Word.

---
+++ Pr21/Pr21/MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Pr21
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически создаваемый конструктором форм
+
+        private System.Windows.Forms.Label lblFourDigitNumber;
+        private System.Windows.Forms.Label lblDistance;
+        private System.Windows.Forms.Label lblString;
+        private System.Windows.Forms.TextBox txtFourDigitNumber;
+        private System.Windows.Forms.TextBox txtDistance;
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblResult;
+
+        private void InitializeComponent()
+        {
+            this.lblFourDigitNumber = new System.Windows.Forms.Label();
+            this.lblDistance = new System.Windows.Forms.Label();
+            this.lblString = new System.Windows.Forms.Label();
+            this.txtFourDigitNumber = new System.Windows.Forms.TextBox();
+            this.txtDistance = new System.Windows.Forms.TextBox();
+            this.txtString = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblFourDigitNumber
+            // 
+            this.lblFourDigitNumber.AutoSize = true;
+            this.lblFourDigitNumber.Font = new System.Drawing.Font("Times New Roman", 15.75F);
+            this.lblFourDigitNumber.Location = new System.Drawing.Point(12, 23);
+            this.lblFourDigitNumber.Name = "lblFourDigitNumber";
+            this.lblFourDigitNumber.Size = new System.Drawing.Size(395, 23);
+            this.lblFourDigitNumber.TabIndex = 0;
+            this.lblFourDigitNumber.Text = "Задание 1:  Введите четырехзначное число:";
+            // 
+            // lblDistance
+            // 
+            this.lblDistance.AutoSize = true;
+            this.lblDistance.Font = new System.Drawing.Font("Times New Roman", 15.75F);
+            this.lblDistance.Location = new System.Drawing.Point(12, 84);
+            this.lblDistance.Name = "lblDistance";
+            this.lblDistance.Size = new System.Drawing.Size(419, 23);
+            this.lblDistance.TabIndex = 2;
+            this.lblDistance.Text = "Задание 2: Введите расстояние в сантиметрах:";
+            // 
+            // lblString
+            // 
+            this.lblString.AutoSize = true;
+            this.lblString.Font = new System.Drawing.Font("Times New Roman", 15.75F);
+            this.lblString.Location = new System.Drawing.Point(12, 145);
+            this.lblString.Name = "lblString";
+            this.lblString.Size = new System.Drawing.Size(244, 23);
+            this.lblString.TabIndex = 4;
+            this.lblString.Text = "Задание 6: Введите строку:";
+            // 
+            // txtFourDigitNumber
+            // 
+            this.txtFourDigitNumber.Font = new System.Drawing.Font("Times New Roman", 15.75F);
+            this.txtFourDigitNumber.Location = new System.Drawing.Point(12, 49);
+            this.txtFourDigitNumber.MaxLength = 4;
+            this.txtFourDigitNumber.Name = "txtFourDigitNumber";
+            this.txtFourDigitNumber.Size = new System.Drawing.Size(204, 32);
+            this.txtFourDigitNumber.TabIndex = 1;
+            // 
+            // txtDistance
+            // 
+            this.txtDistance.Font = new System.Drawing.Font("Times New Roman", 15.75F);
+            this.txtDistance.Location = new System.Drawing.Point(12, 110);
+            this.txtDistance.Name = "txtDistance";
+            this.txtDistance.Size = new System.Drawing.Size(204, 32);
+            this.txtDistance.TabIndex = 3;
+            // 
+            // txtString
+            // 
+            this.txtString.Font = new System.Drawing.Font("Times New Roman", 15.75F);
+            this.txtString.Location = new System.Drawing.Point(12, 171);
+            this.txtString.Multiline = true; // Позволяет вводить многострочный текст
+            this.txtString.Name = "txtString";
+            this.txtString.Size = new System.Drawing.Size(410, 60);
+            this.txtString.TabIndex = 5;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Times New Roman", 15.75F);
+            this.btnGenerate.Location = new System.Drawing.Point(69, 241);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(301, 31);
+            this.btnGenerate.TabIndex = 6;
+            this.btnGenerate.Text = "Создать документ Word";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.Task);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 280);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 324); // Увеличил высоту для лучшего отображения
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtString);
+            this.Controls.Add(this.lblString);
+            this.Controls.Add(this.txtDistance);
+            this.Controls.Add(this.lblDistance);
+            this.Controls.Add(this.txtFourDigitNumber);
+            this.Controls.Add(this.lblFourDigitNumber);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Вариант 17";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ Pr21/Pr21/MainForm.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace Pr21
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void Task(object sender, EventArgs e)
+        {
+            int number;
+            try
+            {
+                number = Convert.ToInt32(txtFourDigitNumber.Text);
+                if (number < 1000 || number > 9999)
+                {
+                    MessageBox.Show("Введите корректное четырехзначное число.");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введите корректное четырехзначное число.");
+                return;
+            }
+
+            int distance;
+            try
+            {
+                distance = Convert.ToInt32(txtDistance.Text);
+                if (distance <= 0)
+                {
+                    MessageBox.Show("Введите корректное (положительное) расстояние в сантиметрах.");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введите корректное (положительное) расстояние в сантиметрах.");
+                return;
+            }
+
+            string str = txtString.Text;
+            if (str.Length < 30)
+            {
+                MessageBox.Show("Строка должна содержать минимум 30 символов. Текущая длина: " + str.Length);
+                return;
+            }
+
+            bool t2 = Task2(number);
+            string result;
+
+            if (t2)
+            {
+                result = "Цифры образуют возрастающую последовательность.";
+            }
+            else
+            {
+                result = "Цифры не образуют возрастающую последовательность.";
+            }
+
+            int metr = distance / 100;
+            string mod = Replace(str, out int count);
+
+            GenerateWordDocument(number, result, metr, str, mod, count);
+        }
+
+        private bool Task2(int number)
+        {
+            int digit = 10;
+            while (number > 0)
+            {
+                int current = number % 10;
+                if (current >= digit)
+                    return false;
+                digit = current;
+                number /= 10;
+            }
+            return true;
+        }
+
+
+        private string Replace(string input, out int count)
+        {
+            char[] chars = input.ToCharArray();
+            count = 0;
+
+            for (int i = 1; i < chars.Length; i += 2)
+            {
+                chars[i] = '%';
+                count++;
+            }
+
+            return new string(chars);
+        }
+
+
+
+        private void GenerateWordDocument(int number, string result, int metr, string str, string mod, int count)
+        {
+            Word.Application wordApp = new Word.Application();
+            Word.Document wordDoc = null;
+            object missing = System.Reflection.Missing.Value;
+
+            try
+            {
+                wordDoc = wordApp.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+                wordDoc.PageSetup.Orientation = Word.WdOrientation.wdOrientPortrait;
+
+                wordDoc.PageSetup.TopMargin = wordApp.InchesToPoints(0.60f);
+                wordDoc.PageSetup.BottomMargin = wordApp.InchesToPoints(0.60f);
+                wordDoc.PageSetup.LeftMargin = wordApp.InchesToPoints(0.80f);
+                wordDoc.PageSetup.RightMargin = wordApp.InchesToPoints(0.59f);
+
+                Word.Paragraph headerParagraph = wordDoc.Content.Paragraphs.Add(ref missing);
+                headerParagraph.Range.Text = "МДК.01.01 Разработка программных модулей";
+                headerParagraph.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                headerParagraph.Range.Font.Size = 18;
+                headerParagraph.Range.Font.Bold = 1;
+                headerParagraph.Range.InsertParagraphAfter();
+
+                Word.Paragraph subHeaderParagraph = wordDoc.Content.Paragraphs.Add(ref missing);
+                subHeaderParagraph.Range.Text = "ПР21 «Программное создание документов MS Word в языке C#»";
+                subHeaderParagraph.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                subHeaderParagraph.Range.Font.Size = 16;
+                subHeaderParagraph.Range.Font.Bold = 0;
+                subHeaderParagraph.Range.InsertParagraphAfter();
+
+                Word.Paragraph studentInfo = wordDoc.Content.Paragraphs.Add(ref missing);
+                studentInfo.Range.Text = "Выполнил Солоников А.С., студент группы ИС-22А";
+                studentInfo.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+                studentInfo.Range.Font.Size = 16;
+                studentInfo.Range.Font.Bold = 0;
+                studentInfo.Range.InsertParagraphAfter();
+
+                Word.Paragraph taskResults = wordDoc.Content.Paragraphs.Add(ref missing);
+                taskResults.Range.Text = $"Задание 1:\nНачальное число: {number}\nРезультат: {result}\n\n" +
+                                         $"Задание 2:\nНачальное расстояние: {metr * 100} см\nПолных метров: {metr}\n\n" +
+                                         $"Задание 3:\nНачальная строка: {str}\nМодифицированная строка: {mod}\n" +
+                                         $"Количество замен: {count}";
+                taskResults.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+                taskResults.Range.Font.Size = 14;
+                taskResults.Range.Font.Bold = 0;
+                taskResults.Range.InsertParagraphAfter();
+
+                string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Pr21Var17.docx";
+                object filename = filePath;
+                wordDoc.SaveAs2(ref filename);
+                wordDoc.Close(ref missing, ref missing, ref missing);
+                wordApp.Quit(ref missing, ref missing, ref missing);
+
+                MessageBox.Show("Документ успешно создан и сохранен.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при создании документа: " + ex.Message);
+            }
+            finally
+            {
+                if (wordDoc != null)
+                {
+                    try 
+                    {
+                        wordDoc.Close(ref missing, ref missing, ref missing);
+                    }
+                    catch 
+                    { 
+
+                    }
+                }
+                if (wordApp != null)
+                {
+                    try 
+                    {
+                        wordApp.Quit(ref missing, ref missing, ref missing);
+                    } 
+                    catch
+                    {
+
+                    }
+                }
+            }
+        }
+
+
+
+
+
+        private void lblFourDigitNumber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDistance_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtString_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
