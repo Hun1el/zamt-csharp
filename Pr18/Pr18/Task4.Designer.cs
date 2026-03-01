226ecf3bbd6c613f3fc7c37776c62989256b3cf2
Anton Solonikov <antonsolonikov@gmail.com>
2026-03-01 16:48:41 +0300

    Pr18/task4. Добавлен дизайн формы для четвертого задания.

--- Pr18/Pr18/Task4.Designer.cs
+++ Pr18/Pr18/Task4.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Pr18
 {
-    partial class Task3
+    partial class Task4
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -42,59 +42,59 @@
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(108, 24);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Задание 3.";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(16, 102);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(449, 29);
-            this.textBox1.TabIndex = 9;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 72);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(422, 24);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Введите строку, содержащую цифры и буквы:";
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Задание 4.";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(174, 176);
+            this.button2.Location = new System.Drawing.Point(166, 197);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(128, 34);
-            this.button2.TabIndex = 12;
+            this.button2.TabIndex = 16;
             this.button2.Text = "Проверить";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(359, 241);
+            this.button1.Location = new System.Drawing.Point(355, 262);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(121, 34);
-            this.button1.TabIndex = 11;
+            this.button1.TabIndex = 15;
             this.button1.Text = "Назад";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 116);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(449, 29);
+            this.textBox1.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(422, 24);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Введите строку, содержащую цифры и буквы:";
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 137);
+            this.label2.Location = new System.Drawing.Point(12, 151);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 24);
-            this.label2.TabIndex = 13;
+            this.label2.TabIndex = 17;
             // 
-            // Task3
+            // Task4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(492, 287);
+            this.ClientSize = new System.Drawing.Size(488, 308);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -106,9 +106,9 @@
             this.Margin = new System.Windows.Forms.Padding(6);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "Task3";
+            this.Name = "Task4";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Задание 3";
+            this.Text = "Задание 4";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,10 +117,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
     }
 }
---
+++ Pr18/Pr18/Task4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr18
+{
+    public partial class Task4 : Form
+    {
+        public Task4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
