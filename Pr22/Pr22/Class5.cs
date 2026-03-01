1461ed5c9e44146f058512ad9cacdcb7910dd475
Anton Solonikov <antonsolonikov@gmail.com>
2026-03-01 20:06:29 +0300

    Pr22. Добавлен проект Pr22 с решенными заданиями

---
+++ Pr22/Pr22/Class5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solonikov
+{
+    public class Class5
+    {
+        public int Task05(string str)
+        {
+            var words = str.Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+    }
+}
